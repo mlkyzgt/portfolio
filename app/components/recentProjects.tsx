48a754963dc08a7beea88edaf2779d37ed7c1c9d
mlkyzgt <mlkyzgt@gmail.com>
2024-04-03 00:17:28 +0300

    Initial commit

--- app/components/recentProjects.tsx
+++ app/components/recentProjects.tsx
@@ -4,9 +4,10 @@ export default function RecentProjects() {
   return (
     <div className="relative bg-white h-[800px] text-[#282938] pt-[50px] pr-[30px] pl-[30px]">
       <div className="pr-[20px] pl-[20px]">
-      <h3 className="text-[#282938] text-[15px] font-medium">
-        Recent Projects</h3>
-      <h1 className="text-[#282938] text-[50px] font-bold">My Portfolio</h1>
+        <h3 className="text-[#282938] text-[15px] font-medium">
+          Recent Projects
+        </h3>
+        <h1 className="text-[#282938] text-[50px] font-bold">My Portfolio</h1>
       </div>
       <div className="absolute top-0 right-0">
         <button className="bg-[#E62872] text-white flex w-[200px] h-[50px] justify-center items-center rounded-[5px] text-[17px] mt-[50px] mr-[50px]">
@@ -19,8 +20,8 @@ export default function RecentProjects() {
         </button>
       </div>
 
-      <div className="grid grid-cols-3 gap-25 mt-[60px] m-[5px] w-[1250px] h-[450px]">
-        <div className="shadow-xl p-[20px] rounded mr-[20px]">
+      <div className="grid grid-cols-3 gap-x-8 gap-y-10 mt-[60px] mx-[auto] w-[1250px] h-[450px]">
+        <div className="shadow-xl p-[20px] rounded">
           <img className="w-full h-[250px] rounded" src="./Image.png" alt="" />
           <h3 className="text-[20px] font-bold mt-[25px]">Ahuse</h3>
           <p className="text-[15px] mt-[10px]">
@@ -40,7 +41,7 @@ export default function RecentProjects() {
           </a>
         </div>
 
-        <div className="shadow-xl p-[20px] rounded mr-[20px]">
+        <div className="shadow-xl p-[20px] rounded">
           <img className="w-full h-[250px] rounded" src="./Image2.png" alt="" />
           <h3 className="text-[20px] font-bold mt-[25px]">App Dashboard</h3>
           <p className="text-[15px] mt-[10px]">
@@ -60,7 +61,7 @@ export default function RecentProjects() {
           </a>
         </div>
 
-        <div className="shadow-xl p-[20px] rounded mr-[20px]">
+        <div className="shadow-xl p-[20px] rounded">
           <img className="w-full h-[250px] rounded" src="./Image3.png" alt="" />
           <h3 className="text-[20px] font-bold mt-[25px]">Easy Rent</h3>
           <p className="text-[15px] mt-[10px]">
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,10 @@ import RecentProjects from "./components/recentProjects";
 import Feedback from "./components/feedback";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
-import "./components/RobotoFont.css"
 export default function Home() {
   return (
    <>
-   <div className="font">
+   <div className="font-['Roboto', sans]">
    <Navbar></Navbar><Main></Main><Skills></Skills><AboutMe></AboutMe><RecentProjects></RecentProjects><Feedback></Feedback><Contact></Contact><Footer></Footer>
    </div>
    </>
