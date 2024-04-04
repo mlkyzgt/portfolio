c28040c63ebf880e69d755e4f961b61d742df90b
mlkyzgt <mlkyzgt@gmail.com>
2024-04-04 23:22:59 +0300

    Changes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -8,59 +8,59 @@ import LinkedinIcon from "./LinkedIn.png";
 export default function Footer() {
   return (
     <footer className="h-[240px] bg-[#F5FCFF] relative">
-      <div className="flex items-center justify-between w-full pt-[60px]">
-        <Image className="h-[40px] w-[185px] ml-16" src="/Logo.png" alt="Logo" width={185} height={40} />
+      <div className="flex items-center justify-between w-full pt-[50px]">
+        <Image className="h-[40px] w-[185px] ml-10" src="/Logo.png" alt="Logo" width={185} height={40} />
       </div>
 
-      <div className="flex items-center justify-center absolute top-0 left-1/2 transform -translate-x-1/2 text-center mt-[60px]">
-        <a className="text-black mx-4" href="#">
+      <div className="flex items-center justify-center absolute top-0 left-1/2 transform -translate-x-1/2 text-center mt-[50px]">
+        <a className="text-black mx-3" href="#">
           Home
         </a>
-        <a className="text-black mx-4" href="#">
+        <a className="text-black mx-3" href="#">
           Portfolio
         </a>
-        <a className="text-black mx-4" href="#">
+        <a className="text-black mx-3" href="#">
           About Me
         </a>
-        <a className="text-black mx-4" href="#">
+        <a className="text-black mx-3" href="#">
           Contact
         </a>
-        <a className="text-black mx-4" href="#">
+        <a className="text-black mx-3" href="#">
           Testimonials
         </a>
-        <a className="text-black mx-4" href="#">
+        <a className="text-black mx-3" href="#">
           Portfolio
         </a>
       </div>
 
-      <div className="flex items-center justify-end absolute top-0 right-0 mt-[60px] mr-16">
+      <div className="flex items-center justify-end absolute top-0 right-0 mt-[50px] mr-10">
         <a href="#">
-          <Image className="w-[25px] mx-4" src={"Twitter.png"} alt="Twitter"/>
+          <Image className="w-[25px]" src={"Twitter.png"} alt="Twitter"/>
         </a>
         <a href="#">
-          <Image className="w-[25px] mx-4" src={"Facebook.png"} alt="Facebook"/>
+          <Image className="w-[25px]" src={"Facebook.png"} alt="Facebook"/>
         </a>
         <a href="#">
-          <Image className="w-[25px] mx-4" src={"Instagram.png"} alt="Instagram"/>
+          <Image className="w-[25px]" src={"Instagram.png"} alt="Instagram"/>
         </a>
         <a href="#">
-          <Image className="w-[25px] mx-4" src={"LinkedIn.png"} alt="Linkedin"/>
+          <Image className="w-[25px]" src={"LinkedIn.png"} alt="Linkedin"/>
         </a>
       </div>
-      <hr className="border-t border-[#ADAEC3] ml-[60px] mr-[60px] mt-[40px]" />
+      <hr className="border-t border-[#ADAEC3] ml-[50px] mr-[50px] mt-[30px]" />
 
-      <p className="text-black absolute bottom-[80px] left-0 mb-4 ml-16">
+      <p className="text-black absolute bottom-[70px] left-0 mb-4 ml-10">
         Made with 💖 by Melike
       </p>
 
-      <div className="flex justify-end absolute bottom-[80px] right-0 mb-4 mr-16">
-        <a className="text-black mr-6" href="#">
+      <div className="flex justify-end absolute bottom-[70px] right-0 mb-4">
+        <a className="text-black mr-4" href="#">
           Privacy Policy
         </a>
-        <a className="text-black mr-6" href="#">
+        <a className="text-black mr-4" href="#">
           Terms of Service
         </a>
-        <a className="text-black mr-16" href="#">
+        <a className="text-black mr-10" href="#">
           Cookies Settings
         </a>
       </div>
