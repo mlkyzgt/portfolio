63829bb4fac97f2a2a28c4e8f3ee8d40c0ee3ac6
mlkyzgt <mlkyzgt@gmail.com>
2024-04-04 23:15:47 +0300

    Changes

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,5 @@
 import Logo from "./Logo.png";
 import Image from "next/image";
-import Link from 'next/link';
 
 export default function Navbar() {
   return (
@@ -9,22 +8,14 @@ export default function Navbar() {
         <div className="flex items-center justify-between w-full">
           <Image
             className="h-[40px] w-[185px] ml-[15px]"
-            src="/Logo.png"
+            src={Logo}
             alt="Logo"
           />
           <div className="flex items-center justify-end">
-            <a className="text-[#5E3BEE] mx-3" href="/main.tsx">
-              Home
-            </a>
-            <a className="text-black mx-3" href="/recentProjects.tsx">
-              Portfolio
-            </a>
-            <a className="text-black mx-3" href="/aboutMe.tsx">
-              About Me
-            </a>
-            <a className="text-black mx-3" href="/feedback.tsx">
-              Testimonials
-            </a>
+            <a className="text-black mx-3" href="/aboutMe.tsx">About Me</a>
+            <a className="text-black mx-3" href="/main.tsx">Home</a>
+            <a className="text-black mx-3" href="/recentProjects.tsx">Portfolio</a>
+            <a className="text-black mx-3" href="/feedback.tsx">Testimonials</a>
           </div>
         </div>
       </nav>
