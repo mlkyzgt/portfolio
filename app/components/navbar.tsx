483907a579b9f49c21fc4640a561001a6c36d815
mlkyzgt <mlkyzgt@gmail.com>
2024-04-04 23:07:08 +0300

    Changes

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,29 +1,34 @@
-import Logo from "./Logo.png";
+import Link from "next/link";
 import Image from "next/image";
+import Logo from "./Logo.png";
 
 export default function Navbar() {
   return (
     <>
       <nav className="bg-white flex h-[60px]">
         <div className="flex items-center justify-between w-full">
-          <Image
-            className="h-[40px] w-[185px] ml-[15px]"
-            src="/Logo.png"
-            alt="Logo"
-          />
+          <div>
+            <Image
+              src={Logo}
+              alt="Logo"
+              width={185}
+              height={40}
+              className="ml-[15px]"
+            />
+          </div>
           <div className="flex items-center justify-end">
-            <a className="text-[#5E3BEE] mx-3" href="/main.tsx">
-              Home
-            </a>
-            <a className="text-black mx-3" href="/recentProjects.tsx">
-              Portfolio
-            </a>
-            <a className="text-black mx-3" href="/aboutMe.tsx">
-              About Me
-            </a>
-            <a className="text-black mx-3" href="/feedback.tsx">
-              Testimonials
-            </a>
+            <Link href="/main">
+              <a className="text-[#5E3BEE] mx-3">Home</a>
+            </Link>
+            <Link href="/recentProjects">
+              <a className="text-black mx-3">Portfolio</a>
+            </Link>
+            <Link href="/aboutMe">
+              <a className="text-black mx-3">About Me</a>
+            </Link>
+            <Link href="/feedback">
+              <a className="text-black mx-3">Testimonials</a>
+            </Link>
           </div>
         </div>
       </nav>
