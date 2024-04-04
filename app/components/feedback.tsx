2ede65e4449843f0776809f60ba0cdac14fba7e2
mlkyzgt <mlkyzgt@gmail.com>
2024-04-04 22:10:42 +0300

    Changes

--- app/components/feedback.tsx
+++ app/components/feedback.tsx
@@ -1,6 +1,8 @@
-import Logo from "./Logo.png";
 import Image from "next/image";
-
+import StarsImage from "./Stars.png";
+import Avatar1Image from "./Avatar1.png";
+import Avatar2Image from "./Avatar2.png";
+import Avatar3Image from "./Avatar3.png";
 
 export default function Feedback() {
   return (
@@ -16,14 +18,14 @@ export default function Feedback() {
         </div>
         <div className="grid grid-cols-3 gap-4 mt-[70px] m-[30px]">
           <div className="bg-[#F5FCFF] p-[20px] rounded border border-solid border-[#006B6A] flex flex-col items-start">
-            <Image className="w-[100px]" src={"Stars.img"} alt="Stars" />
+            <Image className="w-[100px]" src={"/Stars.png"} alt="Stars" />
             <p className="text-[15px] text-[#282938] mt-[20px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse varius enim in eros elementum tristique. Duis cursus,
               mi quis viverra.
             </p>
             <div className="flex items-center justify-start mt-[20px]">
-              <Image className="w-[50px]" src={"Avatar1.img"} alt="Avatar 1" />
+              <Image className="w-[50px]" src={"/Avatar1.png"} alt="Avatar 1" />
               <div className="ml-[10px]">
                 <h3 className="text-[#282938] font-semibold">Dianne Russell</h3>
                 <h3 className="text-[#282938]">Starbucks</h3>
@@ -32,14 +34,14 @@ export default function Feedback() {
           </div>
 
           <div className="bg-[#F5FCFF] p-[20px] rounded border border-solid border-[#006B6A] flex flex-col items-start">
-            <Image className="w-[100px]" src={"Stars.img"} alt="Stars" />
+            <Image className="w-[100px]" src={"/Stars.png"} alt="Stars" />
             <p className="text-[15px] text-[#282938] mt-[20px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse varius enim in eros elementum tristique. Duis cursus,
               mi quis viverra.
             </p>
             <div className="flex items-center justify-start mt-[20px]">
-              <Image className="w-[50px]" src={"Avatar2.img"} alt="Avatar 2" />
+              <Image className="w-[50px]" src={"/Avatar2.png"} alt="Avatar 2" />
               <div className="ml-[10px]">
                 <h3 className="text-[#282938] font-semibold">Kristin Watson</h3>
                 <h3 className="text-[#282938]">Louis Vuitton</h3>
@@ -48,14 +50,14 @@ export default function Feedback() {
           </div>
 
           <div className="bg-[#F5FCFF] p-[20px] rounded border border-solid border-[#006B6A] flex flex-col items-start">
-            <Image className="w-[100px]" src={"Stars.img"} alt="Stars" />
+            <Image className="w-[100px]" src={"/Stars.png"} alt="Stars" />
             <p className="text-[15px] text-[#282938] mt-[20px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse varius enim in eros elementum tristique. Duis cursus,
               mi quis viverra.
             </p>
             <div className="flex items-center justify-start mt-[20px]">
-              <Image className="w-[50px]" src={"Avatar3.img"} alt="Avatar 3" />
+              <Image className="w-[50px]" src={"/Avatar3.png"} alt="Avatar 3" />
               <div className="ml-[10px]">
                 <h3 className="text-[#282938] font-semibold">Kathryn Murphy</h3>
                 <h3 className="text-[#282938]">McDonalds</h3>
