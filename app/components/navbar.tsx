c533ca408e199d3a205f4c71c16672db449a60fe
mlkyzgt <mlkyzgt@gmail.com>
2024-04-05 22:22:58 +0300

    Changes

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,34 +1,23 @@
 import Image from "next/image";
 
-interface CustomLinkProps {
-  href: string;
-  text: string;
-}
-
-const CustomLink: React.FC<CustomLinkProps> = ({ href, text }) => {
-  return <a className="text-black mx-3" href={href}>{text}</a>;
-};
-
-const Navbar: React.FC = () => {
+export default function Navbar() {
   return (
-    <nav className="bg-white flex h-[60px]">
-      <div className="flex items-center justify-between w-full">
-        <div>
+    <>
+      <nav className="bg-white flex h-[60px]">
+        <div className="flex items-center justify-between w-full">
           <Image
             className="h-[40px] w-[185px] ml-[15px]"
             src={"/Logo.png"}
             alt="Logo"
           />
+          <div className="flex items-center justify-end">
+            <a className="text-black mx-3" href="./aboutMe.tsx">About Me</a>
+            <a className="text-black mx-3" href="./main.tsx">Home</a>
+            <a className="text-black mx-3" href="./recentProjects.tsx">Portfolio</a>
+            <a className="text-black mx-3" href="./feedback.tsx">Testimonials</a>
+          </div>
         </div>
-        <div className="flex items-center justify-end">
-          <CustomLink href="/aboutMe.tsx" text="About Me" />
-          <CustomLink href="/main.tsx" text="Home" />
-          <CustomLink href="/recentProjects.tsx" text="Portfolio" />
-          <CustomLink href="/feedback.tsx" text="Testimonials" />
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
-};
-
-export default Navbar;
+}
